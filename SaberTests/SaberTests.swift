91bde47857457b8d0f2a6ba164b39541beba00f1
tamakou <tamakou1217@gmail.com>
2025-10-19 09:36:39 +0900

    Initial Commit

---
+++ SaberTests/SaberTests.swift
@@ -0,0 +1,17 @@
+//
+//  SaberTests.swift
+//  SaberTests
+//
+//  Created by tamakou on 2025/10/19.
+//
+
+import Testing
+@testable import Saber
+
+struct SaberTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
